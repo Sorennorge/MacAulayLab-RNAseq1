db21991d7cb0420bc0b2b0d87c98130c50467ee6
Søren Norge Andreassen <46787229+Sorennorge@users.noreply.github.com>
2022-07-28 16:15:49 +0200

    Update 8.1_Kinases.py

--- 8.1_Kinases.py
+++ 8.1_Kinases.py
@@ -7,14 +7,11 @@ Created on Thu Oct 28 13:11:35 2021
 
 ### Kinases ###
 
-### Kinases ###
-
 import os
 
 from operator import itemgetter
 
-### Files and folders ###
-
+### Folders ###
 
 Folder1 = "Lists"
 Folder2 = "Lists/Count Table"
@@ -22,27 +19,27 @@ Folder3 = "Results"
 Folder4 = "Results/Categories"
 Folder5 = "Results/Categories/Kinases"
 
-
+# If folders doesn't exist -> create #
 if not os.path.exists(Folder3):
     os.mkdir(Folder3)
 else:
     pass
-
 if not os.path.exists(Folder4):
     os.mkdir(Folder4)
 else:
     pass
-
 if not os.path.exists(Folder5):
     os.mkdir(Folder5)
 else:
     pass
 
+### Files ###
+
 Kinase_list_file = "kinases_ensembl_id_list_KEGG.txt"
 
-File3_Male = "Sample1_TPM.csv"
+File1 = "Sample1_TPM.csv"
 
-File3_out = "Sample1_Kinases.csv"
+File1_out = "Sample1_Kinases.csv"
 
 
 ### Global variables ###
@@ -52,9 +49,9 @@ Kinase_list_ensembl = []
 kinases_TPM = {}
 kinases_Genes = {}
 
-Sorted_Male_kinases = []
+Sorted_kinases = []
 
-### load Transport list ###
+### load Kinase list ###
 
 with open(os.path.join(Folder1,Kinase_list_file),'r') as read:
     next(read)
@@ -65,9 +62,9 @@ read.close
 
 ### load count tables ###
 
-## Sample 3 - Male ##
+## Sample 1 ##
 
-with open(os.path.join(Folder2,File3_Male),'r') as read:
+with open(os.path.join(Folder2,File1),'r') as read:
     next(read)
     for line in read:
         line = line.strip().split(";")
@@ -87,19 +84,19 @@ read.close
 ### sort transport lists by value (TPM)
 
 for key, value in sorted(kinases_TPM.items(), key = itemgetter(1,1), reverse = True):
-    Sorted_Male_kinases.append(key)
+    Sorted_kinases.append(key)
     
 ### Save unfiltrated transport lists to files ###
 
-## Sample 3 - Male ##
+## Sample 1  ##
 
 # Ranking variable initialization #
 Rank = 0 
 
 # Save file (sorted based on TPM)
-with open(os.path.join(Folder5, File3_out),'w+') as out:
+with open(os.path.join(Folder5, File1_out),'w+') as out:
     out.write("Ensembl_id;Gene;TPM;Rank\n")
-    for key in Sorted_Male_kinases:
+    for key in Sorted_kinases:
         Rank += 1
         out.write("{};{};{};{}\n".format(key,kinases_Genes[key],kinases_TPM[key],Rank))
 out.close
